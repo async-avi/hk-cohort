f2634682fa6ff8c3e3f025b6b19ea242d415ae7d
avi <avi.contact22@gmail.com>
2023-12-31 21:45:20 +0530

    Done the hospital assignment

--- 0-1/backEnd/express/hospitalExpress/index.js
+++ 0-1/backEnd/express/hospitalExpress/index.js
@@ -37,7 +37,9 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   let newHealthyKidney = req.body;
   users[0].kidneys.push(newHealthyKidney);
-  res.redirect("/");
+  res.json({
+    msg: "Added new kidney",
+  });
 });
 
 app.put("/", (req, res) => {
@@ -46,12 +48,41 @@ app.put("/", (req, res) => {
     if (kidneys[i].healthy === false) {
       kidneys[i].healthy = true;
     }
+    res.status(411);
   }
   res.json({
     msg: "Kidney replaced",
   });
 });
 
-app.delete("/", (req, res) => {});
+app.delete("/", (req, res) => {
+  let kidneys = users[0].kidneys;
+  function checkAtleastOneKidney() {
+    if (kidneys.length > 1) {
+      return true;
+    }
+  }
+  if (checkAtleastOneKidney()) {
+    let newKidneys = [];
+    for (let i = 0; i < kidneys.length; i++) {
+      if (kidneys[i].healthy === false) {
+        newKidneys.push({
+          condition: "95%",
+          healthy: true,
+        });
+      }
+    }
+    kidneys = newKidneys;
+    res.json({
+      msg: "Deleted kidney",
+    });
+    console.log(newKidneys);
+  } else {
+    res.json({
+      error: "User have only one kidney",
+    });
+    res.status(411);
+  }
+});
 
 app.listen(port, () => console.log(`App listening on ${port}`));
