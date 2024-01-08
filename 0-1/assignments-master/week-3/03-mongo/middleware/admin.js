e937d7a45e97e05731477cbeb3c8d7adfbfcd951
avi <avi.contact22@gmail.com>
2024-01-08 22:54:20 +0530

    Intialised admin auth middleware

--- 0-1/assignments-master/week-3/03-mongo/middleware/admin.js
+++ 0-1/assignments-master/week-3/03-mongo/middleware/admin.js
@@ -1,9 +1,17 @@
+const { Admin, User } = require("../db");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
   // Implement admin auth logic
   // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
   const email = req.headers.email;
   const password = req.headers.password;
+  function adminAuth(req, res, next) {
+    let userFound = null;
+    User.find({ email: email }).then(async (user) => {
+      userFound = await user;
+    });
+  }
 }
 
 module.exports = adminMiddleware;
