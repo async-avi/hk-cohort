d57a59f4719f15ed060171e71196951919137a7b
avi <avi.contact22@gmail.com>
2024-01-08 21:02:16 +0530

    Created Models as required

--- 0-1/assignments-master/week-3/03-mongo/db/index.js
+++ 0-1/assignments-master/week-3/03-mongo/db/index.js
@@ -1,27 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const MongoURL =
+  "mongodb+srv://avi:avi123@assignmentmongo.w0nadub.mongodb.net/";
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(MongoURL);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  email: String,
+  password: String,
+  courses: [CourseSchema],
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  title: String,
+  description: String,
+  price: String,
+  author: { type: Schema.Types.username, ref: "Users" },
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
