1de774ff8b6a916c614d53de2ca2e24826d4ab35
avi <avi.contact22@gmail.com>
2024-01-09 18:08:17 +0530

    done the sign in route also

--- 0-1/backEnd/mongo/index.js
+++ 0-1/backEnd/mongo/index.js
@@ -23,31 +23,38 @@ const userSchema = new Schema(
 
 const User = new mongoose.model("User", userSchema);
 
-async function findUser(req, res, next) {
-  let email = req.body.email;
-  let user = await User.find({ email: email });
-  if (user.length > 0) {
-    res.status(406).json({
-      msg: "User already exists",
-    });
-  } else {
-    next();
-  }
-}
-app.use(findUser);
-
 app.post("/signUp", async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  let newUser = new User({
-    email,
-    password,
-  });
-  newUser.save().then(() => {
-    res.status(202).json({
-      msg: "User successfully signed up",
+  let user = await User.find({ email: email });
+  if (user.length == 0) {
+    let newUser = await new User({
+      email,
+      password,
     });
+    newUser.save().then(() =>
+      res.status(202).json({
+        msg: "User successfully created",
+      })
+    );
+  }
+  res.json({
+    msg: "User already exists",
   });
+});
+
+app.post("/signIn", async (req, res) => {
+  let email = req.body.email;
+  let user = await User.find({ email: email });
+  if (user.length > 0) {
+    res.status(200).json({
+      msg: "I drink till I'm drunk, smoke till I high",
+    });
+  } else {
+    res.status(403).json({
+      msg: "Restricted access",
+    });
+  }
 });
 
 app.get(`/`, (req, res) => {
