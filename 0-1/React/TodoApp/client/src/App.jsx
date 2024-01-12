d6b07472d9cdaf698d833649b213e71ecd46f703
avi <avi.contact22@gmail.com>
2024-01-12 18:18:28 +0530

    Done with getting all todos from DB and rendering them

--- 0-1/React/TodoApp/client/src/App.jsx
+++ 0-1/React/TodoApp/client/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const getAndUpdateTodos = async () => {
+    let resp = await axios.get("http://localhost:8000/");
+    let data = await resp.data;
+    setTodos(data);
+  };
+  getAndUpdateTodos();
+
+  const postTodo = async () => {
+    try {
+      let postedTodo = await axios.post("http://localhost:8000/", {
+        title,
+        description: desc,
+      });
+      console.log(postedTodo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // setInterval(async () => {
+
+  const Todo = () => {
+    return todos.map((todo) => {
+      const { title, description, _id } = todo;
+      return (
+        <div key={_id}>
+          <h2>{title}</h2>
+          <h3>{description}</h3>
+        </div>
+      );
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log({
+            title,
+            description: desc,
+          });
+        }}
+      >
+        <h4>Add title</h4>
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" onChange={(e) => setDesc(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>{todos.length}</h3>
+      {/* <Todo /> */}
+      <Todo />;
     </>
-  )
+  );
 }
 
-export default App
+export default App;
