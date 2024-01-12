d294dcd44d8ed089140f7bc2992d36c50c2d73de
avi <avi.contact22@gmail.com>
2024-01-12 17:10:20 +0530

    Declared Todo Model and get route for all todos

--- 0-1/React/TodoApp/Server/db/index.js
+++ 0-1/React/TodoApp/Server/db/index.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
+import { Schema } from "mongoose";
 
-let mongoURL = "mongodb+srv://avi:notsoweak123@learning.jg5tycr.mongodb.net/";
+let mongoURL = "mongodb+srv://avi:notsoweak123@learning.jg5tycr.mongodb.net";
 
 let connectDB = async () => {
   try {
-    let connectionInstance = await mongoose.connect(mongoURL);
+    let connectionInstance = await mongoose.connect(`${mongoURL}/todoApp`);
     console.log(`Connected to database: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("Error connecting to Mongo");
   }
 };
 
-export { connectDB };
+let todoSchema = new Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+  owner: String,
+});
+
+const Todo = new mongoose.model("Todo", todoSchema);
+
+export { connectDB, Todo };
--- 0-1/React/TodoApp/Server/index.js
+++ 0-1/React/TodoApp/Server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { connectDB } from "./db/index.js";
+import { Todo, connectDB } from "./db/index.js";
 
 const app = express();
 const port = 8000;
+
+app.get("/", async (req, res) => {
+  try {
+    let allTodos = await Todo.find({});
+    res.status(200).json(allTodos);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
