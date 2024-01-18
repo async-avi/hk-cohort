f8110b8e583fa4fd4e73739e1ce16a190d35b88f
avi <avi.contact22@gmail.com>
2024-01-18 18:26:04 +0530

    Added drop shadow to the card

--- 0-1/assignments-master/week-5/level-1/CardApp/client/src/styles.js
+++ 0-1/assignments-master/week-5/level-1/CardApp/client/src/styles.js
@@ -1,13 +1,18 @@
 export const styles = {
   card: {
-    border: "5px solid black",
     padding: "20px",
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     width: "20rem",
   },
   button: {
     github: {
       background: "black",
       color: "white",
+      img: {
+        height: "20px",
+        margin: "5px",
+      },
     },
   },
 };
