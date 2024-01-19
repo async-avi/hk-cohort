bc20c74827de268381d13335bbc7de8ba3351e76
avi <avi.contact22@gmail.com>
2024-01-19 13:51:30 +0530

    Done DB connection and declared schemas

--- 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
+++ 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const DB_Name = "pewdemyAdvanced";
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
-
+async function connectDB() {
+  try {
+    let connectionInstance = await mongoose.connect(
+      `mongodb+srv://avi:notsoweak123@learning.jg5tycr.mongodb.net/${DB_Name}`
+    );
+    console.log(
+      `DB connection established: ${connectionInstance.connection.db.databaseName}`
+    );
+  } catch (error) {
+    console.log(`Error connecting to MongoDB: ${error}`);
+  }
+}
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Courses" }],
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
+  purchased: [{ type: mongoose.Schema.Types.ObjectId, ref: "Courses" }],
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  title: String,
+  description: String,
+  price: String,
+  publisher: String,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+  connectDB,
+};
