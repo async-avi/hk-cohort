8e0774fa98916fd41c4923794b62bc93a5af3dc7
avi <avi.contact22@gmail.com>
2024-01-19 20:00:17 +0530

    completed the assignment mongo with jwt

--- 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,19 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const jwt = require("jsonwebtoken");
+const jwtSecret = "user-secret";
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  try {
+    const authorization = req.headers["authorization"];
+    const verify = jwt.verify(authorization, jwtSecret);
+    req.username = await verify.username;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      msg: "Unauthenticated user",
+    });
+  }
 }
 
 module.exports = userMiddleware;
--- 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/routes/user.js
+++ 0-1/assignments-master/week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const { User } = require("../db");
+const { User, Course } = require("../db");
+const jwt = require("jsonwebtoken");
+const jwtSecret = "user-secret";
 
 // User Routes
 router.post("/signup", async (req, res) => {
@@ -21,20 +23,74 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.post("/signin", (req, res) => {
+router.post("/signin", async (req, res) => {
   // Implement admin signup logic
+  try {
+    let findUser = await User.findOne({ username: req.body.username });
+    if (!findUser) {
+      res.status(404).json({ msg: "User not found" });
+    } else {
+      const token = jwt.sign({ username: findUser.username }, jwtSecret);
+      res.status(200).json({
+        Bearer: token,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong with our server" });
+  }
 });
 
-router.get("/courses", (req, res) => {
+router.get("/courses", async (req, res) => {
   // Implement listing all courses logic
+  try {
+    let allCourses = await Course.find({});
+    res.status(200).json({ allCourses });
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong with our server" });
+  }
 });
 
-router.post("/courses/:courseId", userMiddleware, (req, res) => {
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
   // Implement course purchase logic
+  let id = req.params.courseId;
+  const username = req.username;
+  try {
+    let findCourse = await Course.findOne({ _id: id });
+    if (!findCourse) {
+      res.status(404).json({
+        msg: "Cant find the course you are trying to purchase",
+      });
+    } else {
+      let userFound = await User.updateOne(
+        { username: username },
+        { $push: { purchased: findCourse._id } }
+      );
+      res.status(202).json({
+        msg: "Course purchased successfully",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong with our server" });
+  }
 });
 
-router.get("/purchasedCourses", userMiddleware, (req, res) => {
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
   // Implement fetching purchased courses logic
+  const username = req.username;
+  try {
+    const userSearch = await User.findOne({ username: username });
+    let purchasedCourses = [];
+    for (let i = 0; i < userSearch.purchased.length; i++) {
+      const elementId = userSearch.purchased[i]._id;
+      const courseDetail = await Course.findById(elementId);
+      purchasedCourses.push(courseDetail);
+    }
+    res.status(200).json({
+      purchasedCourses: purchasedCourses,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong with our server" });
+  }
 });
 
 module.exports = router;
