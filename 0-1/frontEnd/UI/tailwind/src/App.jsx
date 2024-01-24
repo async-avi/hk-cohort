b3264e0c2c7c2c75ae5bd105be2929c5db6b8119
avi <avi.contact22@gmail.com>
2024-01-24 16:17:40 +0530

    Tailwind first project

---
+++ 0-1/frontEnd/UI/tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <div className="bg-red-600 col-span-3 md:col-span-1">Hello</div>
+        <div className="bg-yellow-400 col-span-3 md:col-span-1">Hello</div>
+        <div className="bg-green-400 col-span-3 md:col-span-1">Hello</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
