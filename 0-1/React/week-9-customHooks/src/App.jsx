9f783f2e7b18947fdbc502c034a308b8e7e0272b
avi <avi.contact22@gmail.com>
2024-01-29 12:45:32 +0530

    Fetching data custom hooks

--- 0-1/React/week-9-customHooks/src/App.jsx
+++ 0-1/React/week-9-customHooks/src/App.jsx
@@ -1,34 +1,60 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [show, setShow] = useState(true);
-  setTimeout(() => {
-    setShow((show) => !show);
-  }, 5000);
+  const todos = useTodos();
   return (
     <>
-      {show ? (
-        <>
-          <MyComponent />
-        </>
-      ) : null}
+      <MyComponent />
+      {todos ? (
+        todos.map((todo) => (
+          <div key={todo.id}>
+            <h1>{todo.title}</h1>
+          </div>
+        ))
+      ) : (
+        <h2>Loading....</h2>
+      )}
     </>
   );
 };
 function MyComponent() {
-  useEffect(() => {
-    console.log("component mounted");
+  // useEffect(() => {
+  //   console.log("component mounted");
 
-    return () => {
-      console.log("component unmounted");
-    };
-  });
+  //   return () => {
+  //     console.log("component unmounted");
+  //   };
+  // });
 
   return (
     <>
       <h3>Hello world!!</h3>
     </>
   );
+}
+
+const todosArr = [
+  {
+    id: 1,
+    title: "Buy milk",
+  },
+  {
+    id: 2,
+    title: "Buy bread",
+  },
+  {
+    id: 3,
+    title: "Buy pan",
+  },
+];
+
+function useTodos() {
+  const [todos, setTodos] = useState(null);
+  setInterval(() => {
+    setTodos(todosArr);
+  }, 1500);
+
+  return todos;
 }
 
 export default App;
