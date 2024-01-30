a8a83268139390343a5ca9d84e3effa1b1c3dfdf
avi <avi.contact22@gmail.com>
2024-01-30 19:30:13 +0530

    Done interfaces

--- 0-1/TS/src/interface.ts
+++ 0-1/TS/src/interface.ts
@@ -14,3 +14,28 @@ const Mahesh: User = {
 };
 
 console.log(isLegalTwo(Mahesh));
+
+interface Shape {
+  color: string;
+}
+
+interface Square extends Shape {
+  sideLength: number;
+}
+
+const TigerBiscuit: Square = { color: "brown", sideLength: 4 };
+console.log(TigerBiscuit);
+
+interface Logger {
+  log(msg: string): void;
+}
+
+class Greetings implements Logger {
+  log(msg: string) {
+    console.log(msg);
+  }
+}
+
+const merryChristmas = new Greetings();
+
+merryChristmas.log("Wish you merry christmas");
