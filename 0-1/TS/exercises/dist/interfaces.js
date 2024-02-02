c2cbf53695a43d5bfd3aacc9ddb476ca89c9e8d2
avi <avi.contact22@gmail.com>
2024-02-02 20:09:49 +0530

    Done the second round of assignments

--- 0-1/TS/exercises/dist/interfaces.js
+++ 0-1/TS/exercises/dist/interfaces.js
@@ -10,3 +10,16 @@ const harry = {
 };
 console.log(jimmy);
 console.log(harry);
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    area() {
+        return Math.floor(Math.PI * this.radius * this.radius);
+    }
+}
+const newCircle = new Circle(7);
+console.log(newCircle.area());
+const search = (arr, searchValue) => arr.includes(searchValue);
+const result1 = search([1, 2, 3, 4], 3);
+console.log(result1);
--- 0-1/TS/exercises/src/interfaces.ts
+++ 0-1/TS/exercises/src/interfaces.ts
@@ -27,3 +27,38 @@ const harry: Animal = {
 
 console.log(jimmy);
 console.log(harry);
+
+//Optional Properties
+
+interface Car {
+  company: string;
+  model: string;
+  year?: number;
+}
+
+interface Cat extends Animal {
+  color: string;
+}
+
+interface Shape {
+  area: () => void;
+}
+
+class Circle implements Shape {
+  constructor(private radius: number) {}
+  area(): number {
+    return Math.floor(Math.PI * this.radius * this.radius);
+  }
+}
+
+const newCircle = new Circle(7);
+console.log(newCircle.area());
+
+interface SearchFunction {
+  (arr: any[], searchValue: any): boolean;
+}
+
+const search: SearchFunction = (arr, searchValue) => arr.includes(searchValue);
+
+const result1: boolean = search([1, 2, 3, 4], 3);
+console.log(result1);
