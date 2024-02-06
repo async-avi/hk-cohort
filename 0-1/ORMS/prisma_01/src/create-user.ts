a7d1cece6ca7df9c978a48328dbdb6e4021986ba
avi <avi.contact22@gmail.com>
2024-02-06 16:46:53 +0530

    Done creating user function

---
+++ 0-1/ORMS/prisma_01/src/create-user.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "avi@gmail.com",
+      name: "avi",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("Query Generated");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
