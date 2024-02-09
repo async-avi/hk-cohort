1e68e6dacff1f4bc2935addb53f8a03b6b8dac62
avi <avi.contact22@gmail.com>
2024-02-09 21:43:20 +0530

    Done todo create

--- 0-1/assignments-master/week-10/1-postgres-simple/src/db/todo.ts
+++ 0-1/assignments-master/week-10/1-postgres-simple/src/db/todo.ts
@@ -9,9 +9,28 @@ import { client } from "..";
  *  id: number
  * }
  */
-export async function createTodo(userId: number, title: string, description: string) {
-    
+export async function createTodo(
+  userId: number,
+  title: string,
+  description: string
+) {
+  try {
+    const query =
+      "INSERT INTO todos (user_id, title, description) VALUES ($1, $2, $3) RETURNING *";
+    const resp = await client.query(query, [userId, title, description]);
+    const respObject = resp.rows[0];
+    console.log({
+      title: respObject.title,
+      description: respObject.description,
+      done: respObject.done,
+      id: respObject.id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+createTodo(1, "Go gym", "go to gym");
 /*
  * mark done as true for this specific todo.
  * Should return a todo object
@@ -22,9 +41,7 @@ export async function createTodo(userId: number, title: string, description: str
  *  id: number
  * }
  */
-export async function updateTodo(todoId: number) {
-
-}
+export async function updateTodo(todoId: number) {}
 
 /*
  *  Get all the todos of a given user
@@ -36,6 +53,4 @@ export async function updateTodo(todoId: number) {
  *  id: number
  * }]
  */
-export async function getTodos(userId: number) {
-
-}
+export async function getTodos(userId: number) {}
