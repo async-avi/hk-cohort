6bcd230d805681ff81396d6976bb25ee0d5ab271
avi <avi.contact22@gmail.com>
2024-02-25 13:44:30 +0530

    Done map

--- 0-1/TS/advanced/src/record&map.ts
+++ 0-1/TS/advanced/src/record&map.ts
@@ -3,13 +3,20 @@ type userScore = {
   league: string;
 };
 
-type userDetails = Record<string, userScore>;
+// type userDetails = Record<string, userScore>;
 
-const aviDetails: userDetails = {
-  ["avi"]: {
-    score: 99980,
-    league: "bronze",
-  },
-};
+// const aviDetails: userDetails = {
+//   ["avi"]: {
+//     score: 99980,
+//     league: "bronze",
+//   },
+// };
 
+// console.log(aviDetails);
+
+const userDetails = new Map<string, userScore>();
+userDetails.set("avi", { score: 99980, league: "bronze" });
+
+// console.log(userDetails);
+const aviDetails = userDetails.get("avi");
 console.log(aviDetails);
